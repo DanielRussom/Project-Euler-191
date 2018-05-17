3f103436f69459650cc2d97892130d9a38049049
Daniel Russom <daniel.russom00@gmail.com>
2018-05-17 03:35:40 +0100

    Valid String Generator
    -Program can now calculate valid strings to 23 days
    -Code restructured to be performed from controller class
    -Added recursive string generation
    -String validity now properly checked

--- src/project191/Controller.java
+++ src/project191/Controller.java
@@ -6,11 +6,12 @@ import java.util.Scanner;
 public class Controller {
 	private ArrayList<String> validStrings = new ArrayList<String>();
 	private int numDays = 0;
-	
+
 	public Controller() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter number of days:");
 		numDays = scan.nextInt();
+		validStrings.add("");
 		evolve();
 	}
 
@@ -18,12 +19,38 @@ public class Controller {
 		if (numDays == 0) {
 			return;
 		}
-		evolve(numDays);
+		evolve(1);
 	}
 
-	private static void evolve(int remainingDays) {
-		
+	private void evolve(int currentDay) {
+		ArrayList<String> nextGeneration = new ArrayList<String>();
+		for (String currentString : validStrings) {
+			nextGeneration.addAll(createNewValidGeneration(currentString));
+		}
+		validStrings = nextGeneration;
+		System.out.println(nextGeneration.size() + " valid matches in " + currentDay + " days.");
+		if (currentDay < numDays) {
+			evolve(currentDay + 1);
+		}
 	}
+
+	public static ArrayList<String> createNewValidGeneration(String currentGeneration) {
+		ArrayList<String> newGeneration = new ArrayList<String>();
+		newGeneration.add(currentGeneration + "O");
+		if (!currentGeneration.contains("L")) {
+			newGeneration.add(currentGeneration + "L");
+		}
+		if (currentGeneration.length() < 2) {
+			newGeneration.add(currentGeneration + "A");
+		} else {
+			String subString = currentGeneration.substring(currentGeneration.length() - 2);
+			if (!subString.equals("AA")) {
+				newGeneration.add(currentGeneration + "A");
+			}
+		}
+		return newGeneration;
+	}
+
 	public static void main(String[] args) {
 		new Controller();
 	}
